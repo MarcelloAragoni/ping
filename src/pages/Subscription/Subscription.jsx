57871d18cf75df0dc913fe2365c9ef0159ec9ff6
Marcello <marcello_aragoni@yahoo.com.br>
2022-03-24 14:09:43 -0300

    Created and imported component subscription

---
+++ src/pages/Subscription/Subscription.jsx
@@ -0,0 +1,15 @@
+
+export default function SubscriptionPage(){
+	return (
+		<div>
+			<div className="info">
+				<h1>PING.</h1>
+				<p>We are lauching soon!</p>
+				<p>Subscribe and get notified</p>
+			</div>
+			<div className="subscription">
+				<input type="text" />
+			</div>			
+		</div>
+	)
+}
