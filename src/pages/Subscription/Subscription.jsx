ab0824c16503e3d5396a2971f47d1ded966a911e
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-01 11:46:30 -0300

    Implemented components to the subscription page.

--- src/pages/Subscription/Subscription.jsx
+++ src/pages/Subscription/Subscription.jsx
@@ -1,14 +1,18 @@
+import Button from "../../Components/Button/Button";
+import Input from "../../Components/Input/input";
+import "./Subscription.scss";
+
 export default function SubscriptionPage() {
   return (
     <>
       <div className="info">
-        <h1>PING.</h1>
-        <p>We are lauching soon!</p>
+        <h1>PING</h1>
+        <p>We are lauching </p>
         <p>Subscribe and get notified</p>
       </div>
       <div className="subscription">
-        <input type="text" />
-        <button></button>
+        <Input name="SubEmail" placeholder="Your email address..." />
+        <Button type="primary" text="Notify Me" />
       </div>
     </>
   );
