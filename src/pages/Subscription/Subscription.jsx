c6603e02a95b1178bc663057d18d8062d647ff05
MarcelloAragoni <32502447+MarcelloAragoni@users.noreply.github.com>
2022-04-01 17:29:04 -0300

    Implemented components to the subscription page. (#4)
    **Trello Task**: https://trello.com/c/DzVHcHwu/6-s
    
    ## Description
    Implemented components and the styles of the subscription page.
    
    ## Screenshot
    ![image](https://user-images.githubusercontent.com/32502447/161287402-d61f0bc8-e067-46dc-b9fa-e67363960aa7.png)

--- src/pages/Subscription/Subscription.jsx
+++ src/pages/Subscription/Subscription.jsx
@@ -1,14 +1,23 @@
+import Button from "../../Components/Button/Button";
+import Input from "../../Components/Input/input";
+
+import "./Subscription.scss";
+
 export default function SubscriptionPage() {
   return (
     <>
       <div className="info">
-        <h1>PING.</h1>
-        <p>We are lauching soon!</p>
+        <h1>
+          PING <span>.</span>
+        </h1>
+        <p>
+          We are lauching <span>soon!</span>
+        </p>
         <p>Subscribe and get notified</p>
       </div>
       <div className="subscription">
-        <input type="text" />
-        <button></button>
+        <Input name="SubEmail" placeholder="Your email address..." />
+        <Button type="primary" text="Notify Me" />
       </div>
     </>
   );
