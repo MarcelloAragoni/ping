e94df621ff3c8540ec3b47fcb1470d7a00447c8f
Marcello <marcello_aragoni@yahoo.com.br>
2022-04-02 17:39:08 -0300

    add state  on input and button.

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -1,5 +1,9 @@
 import "./button.scss";
 
-export default function Button({ text, type }) {
-  return <button className={`button button--${type}`}>{text}</button>;
+export default function Button({ text, type, onClick }) {
+  return (
+    <button onClick={onClick} className={`button button--${type}`}>
+      {text}
+    </button>
+  );
 }
--- src/Components/Input/input.jsx
+++ src/Components/Input/input.jsx
@@ -1,5 +1,23 @@
 import "./input.scss";
 
-export default function Input({ placeholder, name, type = "text" }) {
-  return <input type={type} name={name} id={name} placeholder={placeholder} />;
+export default function Input({
+  placeholder,
+  name,
+  type = "text",
+  value,
+  setInputState,
+}) {
+  function handleChange(event) {
+    setInputState(event.target.value);
+  }
+  return (
+    <input
+      onChange={handleChange}
+      name={name}
+      id={name}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+    />
+  );
 }
